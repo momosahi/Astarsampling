f15c67665ea9d681ae10d53fe539fc2266acd083
Sahi Mohamed Francis Gonsangbeu <mohamedfrancissahi@gmail.com>
2024-01-06 11:21:07 +0100

    Add AStarSampler class

--- main.py
+++ main.py
@@ -4,6 +4,10 @@
 import numpy as np
 
 
+class AStarSampler:
+    pass
+
+
 def gumbel_sample(x):
     """return an independant sample from Gumbel distribution with location 0 and scale 1"""
     return np.random.gumbel(loc=0, scale=1, size=x.shape)
